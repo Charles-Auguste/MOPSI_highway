5511bace09ce146a1f384d56cfe3b90ef28f54b1
Charles-Auguste <charlesaugustegourio@gmail.com>
2022-01-31 16:23:19 +0100

    4ème commit

--- highway_env/envs/mopsi_env.py
+++ highway_env/envs/mopsi_env.py
@@ -1,3 +1,7 @@
+from itertools import repeat, product
+from typing import Tuple
+
+from gym.envs.registration import register
 import numpy as np
 
 from highway_env import utils
@@ -5,3 +9,13 @@ from highway_env.envs.common.abstract import AbstractEnv
 from highway_env.road.lane import LineType, StraightLane, CircularLane, SineLane
 from highway_env.road.road import Road, RoadNetwork
 from highway_env.vehicle.behavior import IDMVehicle
+
+class MopsiEnv(AbstractEnv):
+    pass
+
+
+
+register(
+    id='mopsi-env-v0',
+    entry_point='highway_env.envs:MopsiEnv',
+)
